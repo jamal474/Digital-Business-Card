f07f8b1908cb435642f3f6a9c445a441a3f6ad5f
MD  SHABBIR JAMAL <mdshabbir820141@gmail.com>
2022-06-27 13:20:09 +0530

    end

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,8 +1,15 @@
+import twitter from '../images/Twitter.png'
+import facebook from '../images/Facebook.png'
+import instagram from '../images/Instagram.png'
+import github from '../images/GitHub.png'
 
 function footer() {
     return (
-    <div>
-        <h1>footer</h1>
+    <div className="footer">
+        <a href="https://twitter.com" target = "_blank"><img src = {twitter}/></a>
+        <a href="https://facebook.com" target = "_blank"><img src = {facebook}/></a>
+        <a href="https://instagram.com" target = "_blank"><img src = {instagram}/></a>
+        <a href="https://github.com" target = "_blank"><img src = {github}/></a>
     </div>
     );
 }
